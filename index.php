2a5150cda26f7cb757ad65df46a264dcffde2d2d
marianabanasiak <admin@stronywww-marketing.pl>
2023-11-28 18:03:56 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/*Napisz system (księga gości) która umożliwi logowanie rejestracje użytkowników 
+a także wpisywanie informacji do księgi, edytowanie wpisów księgi, 
+kasowanie wpisów z księgi. 
+Oraz wyświeltenie wszystkich wpisów wszystkich użytkowników na głównej stronie.
+projekt ma być na bieżąco wrzucany do gita, ma być responsywny
+*/
+	echo 'Hello';
+?>
